9c724f530e35cbff57ab3a33930f5ddf493cfbb4
Michal Prihoda <michal@prihoda.net>
2015-08-09 17:44:16 +0200

    Implemented service that will read the acro fields from PDF.

--- src/main/scala/net/prihoda/Service.scala
+++ src/main/scala/net/prihoda/Service.scala
@@ -1,10 +1,23 @@
 package net.prihoda
 
-import akka.http.scaladsl.server.Directives._
+import java.net.URLDecoder
 
-trait Service extends BaseService {
-  val routes = get {
-    log.info("/ executed")
-    complete("OK")
+import akka.http.scaladsl.server.Directives._
+import akka.stream.scaladsl.Sink
+import net.prihoda.pdf.{ FontConfig, AkkaHttpResolverComponent, AcroformFiller }
+import spray.json.{ JsString, JsArray, JsObject }
+
+trait Service extends BaseService with AcroformFiller with AkkaHttpResolverComponent with FontConfig {
+  val routes = {
+    path(Rest)(p => {
+      val uri = URLDecoder.decode(p, "UTF-8")
+      get {
+        val result = for {
+          source <- resolver.resolve(uri)
+          fields <- source.via(pdfAcroFields).runWith(Sink.head)
+        } yield JsObject(Map("fields" -> JsArray(fields.map(JsString.apply).toArray: _*)))
+        complete(result)
+      }
+    })
   }
 }
