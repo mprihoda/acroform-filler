4716acb1a856c3fa4e308cc72eff606041293992
Michal Prihoda <michal@prihoda.net>
2015-08-10 21:54:53 +0200

    Worked around the spray-json UTF-8 parsing from byte array bug.

--- src/main/scala/net/prihoda/Service.scala
+++ src/main/scala/net/prihoda/Service.scala
@@ -18,8 +18,9 @@ trait Service extends BaseService with AcroformFiller with AkkaHttpResolverCompo
         } yield JsObject(Map("fields" -> JsArray(fields.map(JsString.apply).toArray: _*)))
         complete(result)
       } ~ post {
-        entity(as[JsObject])(jsData => {
-          val data = jsData.fields.mapValues {
+        entity(as[String])(body => {
+          import spray.json._
+          val data = body.parseJson.asJsObject.fields.mapValues {
             case JsString(value) => value
             case _               => ""
           }
